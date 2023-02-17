711e65748f1eedf939b95e865bdb1d054799bd74
Abraham Dominic Newton <33906187+abrahamdominic@users.noreply.github.com>
2023-02-17 06:54:50 -0800

    Alx SE Program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+/* sizeof evaluates the size of a variable */
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,3 @@
+0- run C file through the preprocessor and save the result into another file
+1- compile C file without linking and change output file name
+2- generate assembly code and save output file
